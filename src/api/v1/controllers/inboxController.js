0eae4fb0e79506d893d5d8a509b28228c703dfb6
Klassic-Mak <josiahababio56@gmail.com>
2026-09-10 10:31:20 +0000

    --

---
+++ src/api/v1/controllers/inboxController.js
@@ -0,0 +1,3 @@
+import { PrismaClient } from "@prisma/client";
+import asyncHandler from "express-async-handler";
+
---
+++ src/utils/generate_token.js
@@ -0,0 +1,15 @@
+import crypto from "node:crypto";
+
+export function generateToken() {
+  return crypto.randomBytes(32).toString("hex");
+}
+
+export function hashToken(token) {
+  return crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+}
+
+
+console.log(generateToken())
