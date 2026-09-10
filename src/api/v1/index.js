0c389f00cd946d5fe6681cb67a1fd569adf3f190
Klassic-Mak <josiahababio56@gmail.com>
2026-09-10 08:50:40 +0000

    server run

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -1,1 +1,19 @@
 // run app for our inbounder email server
+import express from "express";
+import initRoute from "../v1/routes/initRoute.js";
+import { v1Router } from "./routes/router.js";
+
+const app = express();
+
+
+const HOST = "0.0.0.0";
+const PORT = process.env.PORT || 8080;
+
+
+
+
+app.use(initRoute)
+app.use(v1Router)
+const server = app.listen(PORT, HOST, () => {
+  console.log(`Server running at http://${HOST}:${PORT}`);
+});
