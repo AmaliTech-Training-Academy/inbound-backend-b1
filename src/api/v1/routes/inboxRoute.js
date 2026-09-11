558f7f9c7fd9164c7e6d07f6e99df4f37c3ab05e
Klassic-Mak <josiahababio56@gmail.com>
2026-09-11 12:10:11 +0000

    add [added Inbox Fetch]

--- src/api/v1/routes/inboxRoute.js
+++ src/api/v1/routes/inboxRoute.js
@@ -1,10 +1,16 @@
 import express from 'express'
-import { createInbox } from '../controllers/inboxController.js';
+import { createInbox, getInboxInfo } from '../controllers/inboxController.js';
+import { requireInboxAccess } from '../../../middlewares/requireInboxAccess.js';
 
 const router = express.Router();
 
 router.post('/',
     createInbox
+)
+
+router.post('/info',
+    requireInboxAccess,
+    getInboxInfo
 )
 
 export {router as inboxRouter};
