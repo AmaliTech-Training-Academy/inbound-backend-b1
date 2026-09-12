e0f1c5a1cb71f84a79a635fb39a6ec869cda3c60
Akuzoba <theonlysamboy@gmail.com>
2026-09-12 23:07:31 +0000

    Revert "feat(mail): add inbound message ingestion"
    This reverts commit 8133013452170d9909f01abc6b55e8b0769c097f.

--- src/api/v1/routes/inboundWebhookRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createInboundWebhookController } from "../controllers/inboundWebhookController.js";
-
-export const createInboundWebhookRouter = (io) => {
-  const router = express.Router();
-  router.post("/inbound-email", createInboundWebhookController(io));
-  return router;
-};
