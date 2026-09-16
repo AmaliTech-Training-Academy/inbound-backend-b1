b9177040a08c9ac3fba24470862613ccfb125bec
Klassic-Mak <josiahababio56@gmail.com>
2026-09-16 09:39:25 +0000

    feat: added swagger docs implemetation

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -2,8 +2,11 @@
 import express from "express";
 import initRoute from "../v1/routes/initRoute.js";
 import { v1Router } from "./routes/router.js";
+import swaggerDefinition from "../../configs/swagger.js";
+import swaggerUi from "swagger-ui-express";
 
 const app = express();
+app.set("trust proxy", 1);
 
 
 const HOST = "0.0.0.0";
@@ -11,7 +14,12 @@ const PORT = process.env.PORT || 9001;
 
 
 
-
+app.use(express.json({limit: "1mb"}))
+app.use(express.urlencoded({
+  extended: false,
+  limit: "30mb"
+}))
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDefinition));
 app.use(initRoute)
 app.use(v1Router)
 
