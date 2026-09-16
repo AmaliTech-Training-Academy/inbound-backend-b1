2a3d4e1b0c09974773772386290d69b0d94c5ece
Klassic-Mak <josiahababio56@gmail.com>
2026-09-16 13:45:23 +0000

    feat: added security features for global enpdoint protection

--- src/api/v1/routes/healthRoute.js
+++ src/api/v1/routes/healthRoute.js
@@ -1,10 +1,11 @@
+import { globalRateLimit } from '../../../utils/rateLimit.js';
 import {healthCheckController} from '../controllers/health.js';
 import express from "express";
 
 const router = express.Router();
 
 
-
+router.use(globalRateLimit);
 router.get("/", healthCheckController);
 
 
