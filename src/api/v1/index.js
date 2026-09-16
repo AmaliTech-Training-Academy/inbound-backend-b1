90f992fa2af8c8d44107d1ecf171a325c4168fa4
jhamez-cloud <jameskekeli12@gmail.com>
2026-09-16 12:40:49 +0000

    feat: enhance express app configuration with trust proxy and request limits

--- src/api/v1/index.js
+++ src/api/v1/index.js
@@ -4,13 +4,17 @@ import initRoute from "../v1/routes/initRoute.js";
 import { v1Router } from "./routes/router.js";
 
 const app = express();
-
+app.set("trust proxy", 1);
 
 const HOST = "0.0.0.0";
 const PORT = process.env.PORT || 9001;
 
 
-
+app.use(express.json({limit: "1mb"}))
+app.use(express.urlencoded({
+  extended: false,
+  limit: "30mb"
+}))
 
 app.use(initRoute)
 app.use(v1Router)
@@ -20,3 +24,5 @@ app.use(v1Router)
 const server = app.listen(PORT, HOST, () => {
   console.log(`Server running at http://${HOST}:${PORT}`);
 });
+
+
